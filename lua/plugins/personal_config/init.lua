a277956e695564798945f3c3154b26d2de7e2618
Mathias Capdet <mathias.capdet@tescan.com>
2024-04-26 10:16:01 +0200

    Add new dev plugin

--- lua/plugins/personal_config/init.lua
+++ lua/plugins/personal_config/init.lua
@@ -1,14 +1,30 @@
-local personal_config = vim.fs.normalize("$code/neovim/plugins/personal-basics-nvim")
-if (vim.uv or vim.loop).fs_stat(personal_config) then
-    return {
-        dir = personal_config,
+local dev_plugins_path = "$code/neovim/plugins/"
+
+local dev_plugins = {}
+
+local path = vim.fs.normalize(dev_plugins_path .. "personal-basics-nvim")
+if (vim.uv or vim.loop).fs_stat(path) then
+    table.insert(dev_plugins, {
+        dir = path,
         event = "VeryLazy",
         opts = {
             statusline = false,
             colorscheme = false,
         },
-    }
+    })
+else
+    vim.notify("Could not find personal config plugin in " .. path, vim.log.levels.WARN)
 end
 
-vim.notify("Could not find personal config plugin in " .. personal_config, vim.log.levels.WARN)
-return {}
+path = vim.fs.normalize(dev_plugins_path .. "buffer-opener-nvim")
+if (vim.uv or vim.loop).fs_stat(path) then
+    table.insert(dev_plugins, {
+        dir = path,
+        cmd = { "OpenWeekDailies", "OpenFileBuffers" },
+        config = true,
+    })
+else
+    vim.notify("Could not find personal config plugin in " .. path, vim.log.levels.WARN)
+end
+
+return dev_plugins
