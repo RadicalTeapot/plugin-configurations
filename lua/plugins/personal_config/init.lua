90b1edad92450f0ce41e85acc6d1a0f2ba95ccd1
Mathias Capdet <mathias.capdet@tescan.com>
2024-05-08 14:35:43 +0200

    Add scripts to dev plugins

--- lua/plugins/personal_config/init.lua
+++ lua/plugins/personal_config/init.lua
@@ -24,7 +24,30 @@ if (vim.uv or vim.loop).fs_stat(path) then
         config = true,
     })
 else
-    vim.notify("Could not find personal config plugin in " .. path, vim.log.levels.WARN)
+    vim.notify("Could not find buffer opener plugin in " .. path, vim.log.levels.WARN)
+end
+
+path = vim.fs.normalize(dev_plugins_path .. "personal-scripts-nvim")
+if (vim.uv or vim.loop).fs_stat(path) then
+    table.insert(dev_plugins, {
+        dir = path,
+        keys = {
+            { "<Leader>wd", "<cmd>DimWindowsOn<cr>", desc = "[W]indow [d]im enabled" },
+            { "<Leader>wD", "<cmd>DimWindowsOff<cr>", desc = "[W]indow [d]im disabled" },
+        },
+        cmd = {
+            "OpenDailyNote",
+            "OpenWeeklyNote",
+            "OpenMonthlyNote",
+            "OpenPlanningLayout",
+            "SendToPandoc",
+            "DimWindowsOn",
+            "DimWindowsOff",
+        },
+        config = true,
+    })
+else
+    vim.notify("Could not find personal scripts plugin in " .. path, vim.log.levels.WARN)
 end
 
 return dev_plugins
