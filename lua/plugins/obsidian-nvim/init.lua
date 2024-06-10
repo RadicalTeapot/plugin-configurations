159236ca5e0523ec6ee647f80cb73b546e8faf0c
Mathias Capdet <mathias.capdet@tescan.com>
2024-06-10 13:04:30 +0200

    Change obsidian plugin to latest commit

--- lua/plugins/obsidian-nvim/init.lua
+++ lua/plugins/obsidian-nvim/init.lua
@@ -1,6 +1,6 @@
 return {
     "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
+    -- version = "*", -- recommended, use latest release instead of latest commit (use the latest commit as of 2024-05-27 to fix issue with NeoVim 0.11)
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim",
