60581fb40baf79e78c0c34401f80af32bb9e3132
Mathias Capdet <mathias.capdet@tescan.com>
2024-06-28 15:09:53 +0200

    Update SupercolliderVim config

--- lua/plugins/scnvim/init.lua
+++ lua/plugins/scnvim/init.lua
@@ -1,31 +1,20 @@
+local utils = require("plugins.scnvim.utils")
 return {
     "davidgranstrom/scnvim",
     lazy = true,
     ft = "supercollider",
-    config = function()
-        local scnvim = require("scnvim")
-        local map = scnvim.map
-        local map_expr = scnvim.map_expr
-        scnvim.setup({
-            keymaps = {
-                ["<M-e>"] = map("editor.send_line", { "i", "n" }),
-                ["<C-e>"] = {
-                    map("editor.send_block", { "i", "n" }),
-                    map("editor.send_selection", "x"),
-                },
-                ["<CR>"] = map("postwin.toggle"),
-                ["<M-CR>"] = map("postwin.toggle", "i"),
-                ["<M-L>"] = map("postwin.clear", { "n", "i" }),
-                ["<C-k>"] = map("signature.show", { "n", "i" }),
-                ["<F12>"] = map("sclang.hard_stop", { "n", "x", "i" }),
-                ["<leader>st"] = map("sclang.start"),
-                ["<leader>sk"] = map("sclang.recompile"),
-                ["<F1>"] = map_expr("s.boot"),
-                ["<F2>"] = map_expr("s.meter"),
-            },
-            sclang = {
-                cmd = "C:/Program Files/SuperCollider-3.13.0/sclang.exe",
-            },
-        })
+    opts = {
+        sclang = {
+            cmd = "C:/Program Files/SuperCollider-3.13.0/sclang.exe",
+        },
+        documentation = {
+            cmd = "pandoc",
+        },
+        -- postwin = { float = { enabled = true } },
+    },
+    config = function(_, opts)
+        require("scnvim").setup(opts)
+        vim.g.scnvim_scdoc = 1 -- Browse docs in Neovim
+        utils.set_autcmd()
     end,
 }
