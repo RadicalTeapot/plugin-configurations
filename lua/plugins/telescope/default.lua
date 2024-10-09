379d58e88a53e300940fe2bbe16058877cbdb3a2
Mathias Capdet <mathias.capdet@tescan.com>
2024-10-09 17:43:02 +0200

    Change telescope to use live-grep-args

--- lua/plugins/telescope/default.lua
+++ lua/plugins/telescope/default.lua
@@ -1,15 +1,20 @@
 return {
     cmd = "Telescope",
     keys = function()
+        local telescope = require("telescope")
         local builtin = require("telescope.builtin")
         return {
             { "<leader>tf", builtin.find_files, desc = "[T]elescope find [f]ile" },
             { "<leader>tb", builtin.buffers, desc = "[T]elescope [b]uffers" },
-            { "<leader>tg", builtin.live_grep, desc = "[T]elescope live [g]rep" },
+            { "<leader>tg", telescope.extensions.live_grep_args.live_grep_args, desc = "[T]elescope live [g]rep" },
             { "<leader>th", builtin.help_tags, desc = "[T]elescope [h]elp tags" },
             { "<leader>tr", builtin.oldfiles, desc = "[T]elescope [r]ecent files" },
             { "<leader>tk", builtin.keymaps, desc = "[T]elescope [k]eymaps" },
         }
     end,
-    config = true,
+    config = function(_, opts)
+        local telescope = require("telescope")
+        telescope.setup(opts)
+        telescope.load_extension("live_grep_args")
+    end,
 }
