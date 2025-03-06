cc6a255bbb1f56a0683f7df4bbf7951418b4e7df
Mathias Capdet <mathias.capdet@tescan.com>
2025-03-06 10:39:26 +0100

    Add telescope backlink picker and update notes frontmatter

--- lua/plugins/obsidian-nvim/default.lua
+++ lua/plugins/obsidian-nvim/default.lua
@@ -2,7 +2,7 @@ local vaultPath = vim.fn.expand([[$data/Obsidian vaults/Personal vault]])
 local notesPath = vim.fs.normalize(vaultPath .. [[/**.md]])
 
 local function extract_type_name(input)
-    local pattern = "^%d+ (%w+)$" -- Match any 1 or more digits followed by a single space and 1 or more word characters
+    local pattern = "^%d+ (%w+)$" -- Match any 1 or more digits followed by a single space and 1 or more word characters (e.g., 10 Project)
     local _, _, group = string.find(input, pattern)
 
     if group then
@@ -12,7 +12,7 @@ local function extract_type_name(input)
     end
 end
 
-local get_PARA_metadata = function(relative_note_path)
+local function get_para_type(relative_note_path)
     local parents = relative_note_path:parents()
 
     local type_name = parents[#parents - 1]
@@ -21,20 +21,41 @@ local get_PARA_metadata = function(relative_note_path)
     end
 
     -- Match against PARA types
-    type_name = extract_type_name(string.lower(tostring(type_name["stem"])))
+    type_name = extract_type_name(string.lower(tostring(type_name.name)))
     local types = { project = "project", area = "area", resource = "resource", archive = "archive" }
     if not type_name or not types[type_name] then
         return nil
     end
 
-    local type_value = parents[#parents - 2]
-    if type_value == nil then
+    return type_name
+end
+
+local function get_topic_name(relative_note_path)
+    local parents = relative_note_path:parents()
+
+    local topic_name = parents[#parents - 2]
+    if topic_name == nil then
         return nil
     end
 
-    type_value = tostring(type_value["stem"])
+    -- Use name rather than stem to avoid issues with spaces in name (stem only returns the first part of the string)
+    return tostring(topic_name.name)
+end
 
-    return { name = type_name, value = type_value }
+local build_note_metadata = function(relative_note_path)
+    return {
+        para_type = get_para_type(relative_note_path),
+        topic = get_topic_name(relative_note_path),
+        title = tostring(relative_note_path.stem)
+    }
+end
+
+local function as_wikilink(text)
+    return "[["..text.."]]"
+end
+
+local function is_valid_para_metadata(metadata)
+    return metadata ~= nil and metadata.para_type ~= nil
 end
 
 return {
@@ -69,12 +90,14 @@ return {
             local client = require("obsidian").get_client()
             local workspace = client.current_workspace
 
-            local out = {}
+            local frontmatter_table = {}
             if workspace ~= nil then
                 local path = note.path:relative_to(workspace.path)
-                local metadata = get_PARA_metadata(path)
-                if metadata ~= nil then
-                    out[metadata.name] = metadata.value
+                local metadata = build_note_metadata(path)
+                if is_valid_para_metadata(metadata) then
+                    frontmatter_table["para-type"] = metadata.para_type
+                    frontmatter_table["topic"] = as_wikilink(metadata.topic)
+                    frontmatter_table["title"] = metadata.title
                 end
             end
 
@@ -82,18 +105,18 @@ return {
             -- So here we just make sure those fields are kept in the frontmatter.
             if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
                 for k, v in pairs(note.metadata) do
-                    out[k] = v
+                    frontmatter_table[k] = v
                 end
             end
 
             if note.aliases ~= nil and #note.aliases > 0 then
-                out["aliases"] = note.aliases
+                frontmatter_table["aliases"] = note.aliases
             end
             if note.tags ~= nil and #note.tags > 0 then
-                out["tags"] = note.tags
+                frontmatter_table["tags"] = note.tags
             end
 
-            return out
+            return frontmatter_table
         end,
     },
 }
